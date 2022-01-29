6a6f5df362ee1e29ced88a2b037da3b9ef58b80a
Natalia <alimadeoliveiranatalia@gmail.com>
2022-01-28 23:50:37 -0300

    Definindo Servidor

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
 const express = require('express');
 const app = express();
 app.set('view engine','ejs');
+app.get('/',function(req,res){
+    res.render('pages/home')
+});
+app.listen(3333);
+console.log('Funcionando na porta 3333');
